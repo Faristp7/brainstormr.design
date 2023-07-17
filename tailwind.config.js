beb44b10ff27f84efc00de721c8672efa269d1fd
faris <mrfaristp@gmail.com>
2023-07-17 13:25:13 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js,css}"],
+  darkMode : 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
