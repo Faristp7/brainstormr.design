53da3c23423c38a07ce550ef85b70f0eb1fd14ef
faris <mrfaristp@gmail.com>
2023-07-18 12:01:07 +0530

    signup section completed animation added

--- dist/script.js
+++ dist/script.js
@@ -38,4 +38,62 @@ moonIcon.addEventListener("click", () => {
   themeSwitch();
 });
 
-themeCheck()
+themeCheck();
+
+// const showSignup = () => {
+//   const signupSection = document.getElementById('signup-section')
+//   const signupIdSection = document.getElementById('signupId-section')
+
+//   const signinSection = document.getElementById('signin-section')
+//   const signinIdSection = document.getElementById('signinId-section')
+
+//   signupSection.classList.remove('hidden')
+//   signupIdSection.classList.remove('hidden')
+//   signupSection.classList.add('fade-in');
+//   signupIdSection.classList.add('fade-in');
+
+//   signinSection.classList.add('display-none')
+//   signinIdSection.classList.add('display-none')
+// }
+
+// const showSignin = () => {
+//   const signupSection = document.getElementById('signup-section')
+//   const signupIdSection = document.getElementById('signupId-section')
+
+//   const signinSection = document.getElementById('signin-section')
+//   const signinIdSection = document.getElementById('signinId-section')
+
+//   signinSection.classList.remove('display-none')
+//   signinIdSection.classList.remove('display-none')
+
+//   signupSection.classList.add('hidden')
+//   signupIdSection.classList.add('hidden')
+// }
+
+const showSignup = () => {
+  const signupSection = document.getElementById("signup-section");
+  const signinSection = document.getElementById("signin-section");
+  const signinIdSection = document.getElementById("signinId-section");
+  const signupIdSection = document.getElementById("signupId-section");
+
+  signupSection.classList.remove("hidden");
+  signupIdSection.classList.remove("hidden");
+  signupSection.classList.add("animate__animated", "animate__flipInY");
+
+  signinSection.classList.add("hidden");
+  signinIdSection.classList.add("hidden");
+};
+
+const showSignin = () => {
+  const signupSection = document.getElementById("signup-section");
+  const signinSection = document.getElementById("signin-section");
+  const signinIdSection = document.getElementById("signinId-section");
+  const signupIdSection = document.getElementById("signupId-section");
+
+  signinSection.classList.remove("hidden");
+  signinIdSection.classList.remove('hidden')
+  signinSection.classList.add("animate__animated", "animate__flipInY");
+
+  signupSection.classList.add("hidden");
+  signupIdSection.classList.add('hidden')
+};
